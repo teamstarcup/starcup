74aee18c429b3c9a6e44594545d7aae34f67bc18
little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>
2025-06-15 02:35:48 -0700

    fix: no longer rotate held items unless hovering over a storage window
    Signed-off-by: little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>

--- Content.Client/UserInterface/Systems/Storage/StorageUIController.cs
+++ Content.Client/UserInterface/Systems/Storage/StorageUIController.cs
@@ -181,11 +181,6 @@ public sealed class StorageUIController : UIController, IOnSystemChanged<Storage
         _input.FirstChanceOnKeyEvent += OnMiddleMouse;
     }
 
-    public void OnSystemUnloaded(StorageSystem system)
-    {
-        _input.FirstChanceOnKeyEvent -= OnMiddleMouse;
-    }
-
     /// One might ask, Hey Emo, why are you parsing raw keyboard input just to rotate a rectangle?
     /// The answer is, that input bindings regarding mouse inputs are always intercepted by the UI,
     /// thus, if i want to be able to rotate my damn piece anywhere on the screen,
@@ -225,13 +220,31 @@ public sealed class StorageUIController : UIController, IOnSystemChanged<Storage
         if (!IsDragging && EntityManager.System<HandsSystem>().GetActiveHandEntity() == null)
             return;
 
+        // begin starcup: disable original logic for rotation fix
         //clamp it to a cardinal.
+        // DraggingRotation = (DraggingRotation + Math.PI / 2f).GetCardinalDir().ToAngle();
+        // if (DraggingGhost != null)
+        //     DraggingGhost.Location.Rotation = DraggingRotation;
+
+        // if (IsDragging || UIManager.CurrentlyHovered is StorageWindow)
+        //     keyEvent.Handle();
+        // end starcup
+
+        // begin starcup: only rotate items if we're hovering over a storage window
+        if (DraggingGhost is null && UIManager.CurrentlyHovered is not StorageWindow)
+            return;
+
         DraggingRotation = (DraggingRotation + Math.PI / 2f).GetCardinalDir().ToAngle();
-        if (DraggingGhost != null)
+        if (DraggingGhost is not null)
             DraggingGhost.Location.Rotation = DraggingRotation;
 
-        if (IsDragging || UIManager.CurrentlyHovered is StorageWindow)
-            keyEvent.Handle();
+        keyEvent.Handle();
+        // end starcup
+    }
+
+    public void OnSystemUnloaded(StorageSystem system)
+    {
+        _input.FirstChanceOnKeyEvent -= OnMiddleMouse;
     }
 
     private void OnPiecePressed(GUIBoundKeyEventArgs args, StorageWindow window, ItemGridPiece control)
