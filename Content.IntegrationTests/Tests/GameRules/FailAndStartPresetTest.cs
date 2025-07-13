eac359b6a584657c0865db367965a59c79747a22
PrincessPengy <114524259+princesspengy@users.noreply.github.com>
2025-07-13 01:24:17 -0700

    fix default round cvar in test

--- Content.IntegrationTests/Tests/GameRules/FailAndStartPresetTest.cs
+++ Content.IntegrationTests/Tests/GameRules/FailAndStartPresetTest.cs
@@ -74,7 +74,7 @@ public sealed class FailAndStartPresetTest
 
         Assert.That(server.CfgMan.GetCVar(CCVars.GridFill), Is.False);
         Assert.That(server.CfgMan.GetCVar(CCVars.GameLobbyFallbackEnabled), Is.True);
-        Assert.That(server.CfgMan.GetCVar(CCVars.GameLobbyDefaultPreset), Is.EqualTo("secret"));
+        Assert.That(server.CfgMan.GetCVar(CCVars.GameLobbyDefaultPreset), Is.EqualTo("greenshift")); // starcup: change default to greenshift
         server.CfgMan.SetCVar(CCVars.GridFill, true);
         server.CfgMan.SetCVar(CCVars.GameLobbyFallbackEnabled, false);
         server.CfgMan.SetCVar(CCVars.GameLobbyDefaultPreset, "TestPreset");
