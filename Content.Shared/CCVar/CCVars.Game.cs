347247bada7be3675febba19015632db5cbb2e0e
PrincessPengy <114524259+princesspengy@users.noreply.github.com>
2025-07-13 00:42:25 -0700

    gamemode changes

--- Content.Shared/CCVar/CCVars.Game.cs
+++ Content.Shared/CCVar/CCVars.Game.cs
@@ -33,7 +33,7 @@ public sealed partial class CCVars
     ///     Controls the default game preset.
     /// </summary>
     public static readonly CVarDef<string>
-        GameLobbyDefaultPreset = CVarDef.Create("game.defaultpreset", "secret", CVar.ARCHIVE);
+        GameLobbyDefaultPreset = CVarDef.Create("game.defaultpreset", "greenshift", CVar.ARCHIVE); // starcup: make greenshift default
 
     /// <summary>
     ///     Controls if the game can force a different preset if the current preset's criteria are not met.
