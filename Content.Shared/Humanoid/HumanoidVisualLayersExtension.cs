bc658bce229a8d3bd21a581f92a0edfdeb68af80
PrincessPengy <114524259+princesspengy@users.noreply.github.com>
2025-07-14 03:10:42 -0700

    Allows Markings to Set Custom Layers for Specific Sprites (#742)
    ---------
    
    Co-authored-by: Tk420634 <Tk420634@gmail.com>
    Co-authored-by: CASEY GUNGEON <bound2theground@gmail.com>

--- Content.Shared/Humanoid/HumanoidVisualLayersExtension.cs
+++ Content.Shared/Humanoid/HumanoidVisualLayersExtension.cs
@@ -62,6 +62,8 @@ namespace Content.Shared.Humanoid
                 case HumanoidVisualLayers.Chest:
                     yield return HumanoidVisualLayers.Chest;
                     yield return HumanoidVisualLayers.Tail;
+                    yield return HumanoidVisualLayers.TailBehind;
+                    yield return HumanoidVisualLayers.TailOversuit;
                     break;
                 default:
                     yield break;
