8437b5ccb1f32d886f09716fdd05a49e49644eb9
PrincessPengy <114524259+princesspengy@users.noreply.github.com>
2025-09-09 22:52:02 -0700

    Revert "fix event preferences panel (#704)"
    This reverts commit 40d9f7ebb5c7f3b510d0f82fda28151e362dbc29.

--- Content.Shared/_CD/Admin/EventPreferencePanelEuiState.cs
+++ Content.Shared/_CD/Admin/EventPreferencePanelEuiState.cs
@@ -1,7 +1,6 @@
 ﻿using Content.Shared.Eui;
 using Content.Shared.Preferences;
 using Content.Shared.Roles;
-using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared._CD.Admin;
@@ -10,9 +9,9 @@ namespace Content.Shared._CD.Admin;
 public sealed class EventPreferencePanelEuiState(
     string username,
     HumanoidCharacterProfile preferences,
-    List<ProtoId<AntagPrototype>> visibleAntagPrototypes) : EuiStateBase
+    List<AntagPrototype> visibleAntagPrototypes) : EuiStateBase
 {
     public readonly string Username = username;
     public readonly HumanoidCharacterProfile Preferences = preferences;
-    public readonly List<ProtoId<AntagPrototype>> VisibleAntagPrototypes = visibleAntagPrototypes;
+    public readonly List<AntagPrototype> VisibleAntagPrototypes = visibleAntagPrototypes;
 }
