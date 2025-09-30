d6d9ded88889f5df758c90cc261e0789d984a272
KoboldCatgirl <143960193+KoboldCatgirl@users.noreply.github.com>
2025-09-29 19:58:00 -0700

    added rotation system for reversed cards

---
+++ Content.Shared/_starcup/Cards/Rotation/SparotCardRotationComponent.cs
@@ -0,0 +1,12 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._starcup.Cards.Rotation;
+
+/// <summary>
+/// Makes sp/tarot cards go spin, for reversed cards!
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class SparotCardRotationComponent : Component
+{
+
+}
