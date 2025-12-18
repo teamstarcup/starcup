23254d450602f7c6daa64987546a0f10e1c78471
little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>
2025-12-17 16:45:38 -0800

    feat: shift change shuttle
    Horrendous vomit code here but a deadline is a deadline; fix later
    
    Signed-off-by: little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>

--- Content.Shared/_starcup/CCVars/SCCVars.ShiftChangeShuttle.cs
+++ Content.Shared/_starcup/CCVars/SCCVars.ShiftChangeShuttle.cs
@@ -2,11 +2,10 @@ using Robust.Shared.Configuration;
 
 namespace Content.Shared._starcup.CCVars;
 
-[CVarDefs]
 public sealed partial class SCCVars
 {
     ///<summary>
     ///    Toggles whether the player sprints or walks by default.
     ///</summary>
-    public static readonly CVarDef<bool> WalkByDefault = CVarDef.Create("control.walk_by_default", false, CVar.CLIENT | CVar.ARCHIVE | CVar.REPLICATED);
+    public static readonly CVarDef<int> ShiftChangeShuttleTravelTime = CVarDef.Create("shift_change_shuttle.travel_time", 60, CVar.SERVER | CVar.REPLICATED);
 }
