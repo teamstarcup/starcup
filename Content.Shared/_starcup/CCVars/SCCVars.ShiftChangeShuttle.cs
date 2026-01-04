3adc7186e82e7de3d733b590c74f13ca38a0f0ed
little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>
2026-01-03 21:36:44 -0800

    Update Content.Shared/_starcup/CCVars/SCCVars.ShiftChangeShuttle.cs

--- Content.Shared/_starcup/CCVars/SCCVars.ShiftChangeShuttle.cs
+++ Content.Shared/_starcup/CCVars/SCCVars.ShiftChangeShuttle.cs
@@ -5,7 +5,7 @@ namespace Content.Shared._starcup.CCVars;
 public sealed partial class SCCVars
 {
     ///<summary>
-    ///    Provides the time it takes for Latestart's shift change shuttle to arrive at the station.
+    ///    Provides the time it takes for the shift change shuttle to arrive at the station.
     ///</summary>
     public static readonly CVarDef<int> ShiftChangeShuttleTravelTime = CVarDef.Create("shift_change_shuttle.travel_time", 240, CVar.SERVER | CVar.REPLICATED);
 }
