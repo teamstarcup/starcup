60d6c3f3db7cd5d3729c4c10509e41bc9051591c
PrincessPengy <114524259+princesspengy@users.noreply.github.com>
2026-03-04 21:25:57 -0800

    new skeleton accent

---
+++ Content.Server/_starcup/Speech/Components/NewSkeletonAccentComponent.cs
@@ -0,0 +1,8 @@
+using Content.Server._starcup.Speech.EntitySystems;
+
+namespace Content.Server._starcup.Speech.Components;
+
+[RegisterComponent]
+[Access(typeof(NewSkeletonAccentSystem))]
+public sealed partial class NewSkeletonAccentComponent : Component
+{ }
