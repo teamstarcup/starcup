16da8d6b7aa173090385a4f4597b21283c89d649
Psymbiote <juanfjust91@gmail.com>
2026-05-21 16:18:45 -0300

    Cherry-picked #1009 from The Den.

--- Content.Server/StationEvents/Components/VentCrittersRuleComponent.cs
+++ Content.Server/StationEvents/Components/VentCrittersRuleComponent.cs
@@ -1,17 +1,65 @@
-﻿using Content.Server.StationEvents.Events;
+// SPDX-FileCopyrightText: 2023 DrSmugleaf
+// SPDX-FileCopyrightText: 2023 Nemanja
+// SPDX-FileCopyrightText: 2023 Nim
+// SPDX-FileCopyrightText: 2023 Slava0135
+// SPDX-FileCopyrightText: 2023 metalgearsloth
+// SPDX-FileCopyrightText: 2025 Jakumba
+// SPDX-FileCopyrightText: 2025 empty0set
+// SPDX-FileCopyrightText: 2025 sleepyyapril
+//
+// SPDX-License-Identifier: MIT
+
+using Content.Server.StationEvents.Events;
+using Content.Shared.EntityTable.EntitySelectors;
 using Content.Shared.Storage;
+using Robust.Shared.Audio;
+using Robust.Shared.Map; // DeltaV
 
 namespace Content.Server.StationEvents.Components;
 
 [RegisterComponent, Access(typeof(VentCrittersRule))]
 public sealed partial class VentCrittersRuleComponent : Component
 {
-    [DataField("entries")]
-    public List<EntitySpawnEntry> Entries = new();
+    // DeltaV: Replaced by Table
+    //[DataField("entries")]
+    //public List<EntitySpawnEntry> Entries = new();
+
+    /// <summary>
+    /// DeltaV: Table of possible entities to spawn.
+    /// </summary>
+    [DataField(required: true)]
+    public EntityTableSelector Table = default!;
 
     /// <summary>
     /// At least one special entry is guaranteed to spawn
     /// </summary>
     [DataField("specialEntries")]
     public List<EntitySpawnEntry> SpecialEntries = new();
+
+    /// <summary>
+    /// DeltaV: The location of the vent that got picked.
+    /// </summary>
+    [ViewVariables]
+    public EntityCoordinates? Location;
+
+    /// <summary>
+    /// DeltaV: Base minimum number of critters to spawn.
+    /// </summary>
+    [DataField]
+    public int Min = 4;
+
+    /// <summary>
+    /// DeltaV: Base maximum number of critters to spawn.
+    /// </summary>
+    [DataField]
+    public int Max = 5;
+
+    /// <summary>
+    /// DeltaV: Min and max get multiplied by the player count then divided by this.
+    /// </summary>
+    [DataField]
+    public int PlayerRatio = 20;
+
+    [DataField("sound")]
+    public SoundSpecifier? Sound = new SoundPathSpecifier("/Audio/_DEN/VentCritters/vent_harmless_critter.ogg");
 }
--- Content.Server/StationEvents/Events/VentCrittersRule.cs
+++ Content.Server/StationEvents/Events/VentCrittersRule.cs
@@ -1,12 +1,34 @@
+// SPDX-FileCopyrightText: 2023 Nemanja
+// SPDX-FileCopyrightText: 2023 Nim
+// SPDX-FileCopyrightText: 2023 Slava0135
+// SPDX-FileCopyrightText: 2023 metalgearsloth
+// SPDX-FileCopyrightText: 2024 VMSolidus
+// SPDX-FileCopyrightText: 2024 deltanedas
+// SPDX-FileCopyrightText: 2025 Jakumba
+// SPDX-FileCopyrightText: 2025 empty0set
+// SPDX-FileCopyrightText: 2025 sleepyyapril
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.StationEvents.Components;
+using Content.Server.Antag;
+using Content.Server.Pinpointer;
+using Content.Shared.EntityTable;
 using Content.Shared.GameTicking.Components;
-using Content.Shared.Station.Components;
-using Content.Shared.Storage;
+using Content.Server.Station.Components;
 using Robust.Shared.Map;
+using Robust.Shared.Player;
 using Robust.Shared.Random;
 
 namespace Content.Server.StationEvents.Events;
 
+/// <summary>
+/// DeltaV: Reworked vent critters to spawn a number of mobs at a single telegraphed location.
+/// This gives players time to run away and let sec do their job.
+/// </summary>
+/// <remarks>
+/// This entire file is rewritten, ignore upstream changes.
+/// </remarks>
 public sealed class VentCrittersRule : StationEventSystem<VentCrittersRuleComponent>
 {
     /*
@@ -14,48 +36,102 @@ public sealed class VentCrittersRule : StationEventSystem<VentCrittersRuleCompon
      * USE THE PROTOTYPE.
      */
 
-    protected override void Started(EntityUid uid, VentCrittersRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
-    {
-        base.Started(uid, component, gameRule, args);
+    [Dependency] private readonly AntagSelectionSystem _antag = default!;
+    [Dependency] private readonly EntityTableSystem _entityTable = default!;
+    [Dependency] private readonly ISharedPlayerManager _player = default!;
+    [Dependency] private readonly NavMapSystem _navMap = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
 
-        if (!TryGetRandomStation(out var station))
+    private List<Tuple<EntityUid, VentCritterSpawnLocationComponent, EntityCoordinates>> _locations = new();
+    private Tuple<EntityUid, VentCritterSpawnLocationComponent, EntityCoordinates>? _location;
+
+    protected override void Added(EntityUid uid, VentCrittersRuleComponent comp, GameRuleComponent gameRule, GameRuleAddedEvent args)
+    {
+        PickLocation(comp);
+        if (_location == null)
         {
+            ForceEndSelf(uid, gameRule);
             return;
         }
 
-        var locations = EntityQueryEnumerator<VentCritterSpawnLocationComponent, TransformComponent>();
-        var validLocations = new List<EntityCoordinates>();
-        while (locations.MoveNext(out _, out _, out var transform))
+        if (comp.Location is not { } coords)
         {
-            if (!transform.Anchored)
-                continue;
+            ForceEndSelf(uid, gameRule);
+            return;
+        }
 
-            if (CompOrNull<StationMemberComponent>(transform.GridUid)?.Station == station)
+        var mapCoords = _transform.ToMapCoordinates(coords);
+        if (!_navMap.TryGetNearestBeacon(mapCoords, out var beacon, out _))
+            return;
+
+        var nearest = beacon?.Comp?.Text!;
+        // Our announcement system is a boolean, instead of taking a localisation string.
+        Comp<StationEventComponent>(uid).StartAnnouncement = true; // Loc.GetString("station-event-vent-creatures-start-announcement-deltav", ("location", nearest));
+
+        base.Audio.PlayPvs(comp.Sound, _location.Item1);
+
+        base.Added(uid, comp, gameRule, args);
+    }
+
+    protected override void Ended(EntityUid uid, VentCrittersRuleComponent comp, GameRuleComponent gameRule, GameRuleEndedEvent args)
+    {
+        if (_location == null)
+            return;
+
+        base.Ended(uid, comp, gameRule, args);
+
+        if (comp.Location is not { } coords)
+            return;
+
+        Spawn("AdminInstantEffectSmoke10", _location.Item3);
+
+        SpawnCritters(comp, coords);
+    }
+
+    private void SpawnCritters(VentCrittersRuleComponent comp, EntityCoordinates coords)
+    {
+        var players = _antag.GetTotalPlayerCount(_player.Sessions);
+        var min = comp.Min * players / comp.PlayerRatio;
+        var max = comp.Max * players / comp.PlayerRatio;
+        var count = Math.Max(RobustRandom.Next(min, max), 1);
+
+        for (int i = 0; i < count; i++)
+        {
+            foreach (var spawn in _entityTable.GetSpawns(comp.Table))
             {
-                validLocations.Add(transform.Coordinates);
-                foreach (var spawn in EntitySpawnCollection.GetSpawns(component.Entries, RobustRandom))
-                {
-                    Spawn(spawn, transform.Coordinates);
-                }
+                Spawn(spawn, coords);
             }
         }
 
-        if (component.SpecialEntries.Count == 0 || validLocations.Count == 0)
-        {
+        if (comp.SpecialEntries.Count == 0)
             return;
-        }
 
         // guaranteed spawn
-        var specialEntry = RobustRandom.Pick(component.SpecialEntries);
-        var specialSpawn = RobustRandom.Pick(validLocations);
-        Spawn(specialEntry.PrototypeId, specialSpawn);
+        var specialEntry = RobustRandom.Pick(comp.SpecialEntries);
+        Spawn(specialEntry.PrototypeId, coords);
+    }
 
-        foreach (var location in validLocations)
+
+    private void PickLocation(VentCrittersRuleComponent comp)
+    {
+        if (!TryGetRandomStation(out var station))
+            return;
+
+        var locations = EntityQueryEnumerator<VentCritterSpawnLocationComponent, TransformComponent>();
+        _locations.Clear();
+        while (locations.MoveNext(out var uid, out var spawnLocation, out var transform))
         {
-            foreach (var spawn in EntitySpawnCollection.GetSpawns(component.SpecialEntries, RobustRandom))
+            if (CompOrNull<StationMemberComponent>(transform.GridUid)?.Station == station && spawnLocation.CanSpawn)
             {
-                Spawn(spawn, location);
+                _locations.Add(new Tuple<EntityUid, VentCritterSpawnLocationComponent, EntityCoordinates>(uid, spawnLocation, transform.Coordinates));
             }
         }
+
+        if (_locations.Count == 0)
+            return;
+
+        _location = RobustRandom.Pick(_locations);
+
+        comp.Location = _location.Item3;
     }
 }
