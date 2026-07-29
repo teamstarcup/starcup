961c934e0f55f1af41eb106f654c60257ecb867b
Psymbiote <juanfjust91@gmail.com>
2026-07-28 21:34:36 -0300

    Ported ovinia from Macrocosm

--- Content.Shared/Interaction/InteractionPopupSystem.cs
+++ Content.Shared/Interaction/InteractionPopupSystem.cs
@@ -1,3 +1,4 @@
+using Content.Shared._MACRO.Interaction;
 using Content.Shared.Bed.Sleep;
 using Content.Shared.IdentityManagement;
 using Content.Shared.Interaction.Components;
@@ -5,6 +6,7 @@ using Content.Shared.Interaction.Events;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Popups;
+using Content.Shared.Whitelist;
 using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Network;
@@ -16,6 +18,7 @@ namespace Content.Shared.Interaction;
 
 public sealed class InteractionPopupSystem : EntitySystem
 {
+    [Dependency] private EntityWhitelistSystem _entityWhitelist = null!;
     [Dependency] private readonly IGameTiming _gameTiming = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
@@ -91,7 +94,17 @@ public sealed class InteractionPopupSystem : EntitySystem
         if (_netMan.IsClient && !predict)
             return;
 
-        if (_random.Prob(component.SuccessChance))
+        // MACRO ADD: PettingModifier
+        // TODO: this is a very generic system. we probably need a whitelist of some kind
+        var successChance = component.SuccessChance;
+
+        if (TryComp<PettingChanceModifierComponent>(uid, out var pettingChance)
+            && _entityWhitelist.IsWhitelistPassOrNull(pettingChance.TargetWhitelist, target)
+            && _entityWhitelist.IsWhitelistFailOrNull(pettingChance.TargetBlacklist, target))
+            successChance *= pettingChance.Modifier;
+        // MACRO END
+
+        if (_random.Prob(successChance)) // MACRO: component.SuccessChance -> successChance
         {
             if (component.InteractSuccessString != null)
                 msg = Loc.GetString(component.InteractSuccessString, ("target", Identity.Entity(uid, EntityManager))); // Success message (localized).
--- Resources/Locale/en-US/_MACRO/chat/emotes.ftl
+++
@@ -1,9 +0,0 @@
-# Names
-chat-emote-name-baa = Baa
-chat-emote-name-moo = Moo
-chat-emote-name-hiss = Hiss
-
-# Message
-chat-emote-msg-baa = baas.
-chat-emote-msg-moo = moos.
-chat-emote-msg-hiss = hisses.
