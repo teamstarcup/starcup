f7b9f9f553ff921606beeb007b502ddcaf099110
KoboldCatgirl <143960193+KoboldCatgirl@users.noreply.github.com>
2026-07-30 14:06:09 -0700

    ports Den's (with some of DeltaV's attempted port of EE's) Better Narcolepsy

--- Content.Shared/Traits/Assorted/NarcolepsyComponent.cs
+++ Content.Shared/Traits/Assorted/NarcolepsyComponent.cs
@@ -1,44 +1,48 @@
-﻿using Robust.Shared.GameStates;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+﻿// SPDX-FileCopyrightText: 2022 Moony <moony@hellomouse.net>
+// SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2023 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2024 Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later AND MIT
 
-namespace Content.Shared.Traits.Assorted;
+using System.Numerics;
+
+namespace Content.Server.Traits.Assorted;
 
 /// <summary>
 /// This is used for the narcolepsy trait.
 /// </summary>
-[RegisterComponent, NetworkedComponent]
-[AutoGenerateComponentState(fieldDeltas: true), AutoGenerateComponentPause]
-[Access(typeof(NarcolepsySystem))]
+[RegisterComponent, Access(typeof(NarcolepsySystem))]
 public sealed partial class NarcolepsyComponent : Component
 {
     /// <summary>
-    /// The maximum time between incidents.
+    /// The random time between incidents, (min, max).
     /// </summary>
-    [DataField(required: true), AutoNetworkedField]
-    public TimeSpan MaxTimeBetweenIncidents;
+    [DataField("timeBetweenIncidents", required: true)]
+    public Vector2 TimeBetweenIncidents { get; private set; }
 
     /// <summary>
-    /// The minimum time between incidents.
+    /// The duration of incidents, (min, max).
     /// </summary>
-    [DataField(required: true), AutoNetworkedField]
-    public TimeSpan MinTimeBetweenIncidents;
+    [DataField("durationOfIncident", required: true)]
+    public Vector2 DurationOfIncident { get; private set; }
+
+    [DataField]
+    public float NextIncidentTime;
 
     /// <summary>
-    /// The maximum duration of incidents.
+    ///     Locales for popups shown when the entity is about to fall asleep/is waking up.
+    ///     They are fetched in the format of "(base)-(random number between 1 and count)", e.g. "narcolepsy-warning-popup-3".
     /// </summary>
-    [DataField(required: true), AutoNetworkedField]
-    public TimeSpan MaxDurationOfIncident;
+    [DataField]
+    public string WarningLocaleBase = "narcolepsy-warning-popup", WakeupLocaleBase = "narcolepsy-wakeup-popup";
 
-    /// <summary>
-    /// The minimum duration of incidents.
-    /// </summary>
-    [DataField(required: true), AutoNetworkedField]
-    public TimeSpan MinDurationOfIncident;
+    [DataField]
+    public int WarningLocaleCount = 5, WakeupLocaleCount = 3;
 
-    /// <summary>
-    /// Next time indcident happens.
-    /// </summary>
-    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
-    [AutoNetworkedField, AutoPausedField]
-    public TimeSpan NextIncidentTime = TimeSpan.Zero;
+    [DataField]
+    public float TimeBeforeWarning = 20f, WarningChancePerSecond = 0.25f;
+
+    public float LastWarningRollTime = float.MaxValue;
 }
